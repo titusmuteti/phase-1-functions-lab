acfe53494178c06ff39143960b08eabd0c042e1e
Titus Mumo <titusmumo@gmail.com>
2022-08-17 15:59:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const scuberHeadquater = 42;
+
+function distanceFromHqInBlocks(pickUpPoint) {
+   return (pickUpPoint> scuberHeadquater) ? (pickUpPoint-scuberHeadquater) : (scuberHeadquater - pickUpPoint);   
+}
+
+function distanceFromHqInFeet(pickUpPoint) {
+    return (pickUpPoint> scuberHeadquater)? (pickUpPoint-scuberHeadquater)*264 : (scuberHeadquater - pickUpPoint)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (destination > start)? (destination-start)*264 : (start-destination)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet<400) {
+        return 0;
+    }else if (distanceInFeet>400 && distanceInFeet<2000) {
+        return 2.56;
+    } else if (distanceInFeet > 2000 && distanceInFeet< 2500) {
+        return 25;
+    } else if (distanceInFeet > 2500) {
+        return 'cannot travel that far';
+    }
+}
